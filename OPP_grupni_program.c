0b47a767dde7be07fe50976082b2a5feda3660ac
Malnar Matej <76701775+MalnarM6@users.noreply.github.com>
2023-02-03 11:59:20 +0100

    Update OPP_grupni_program.c

--- OPP_grupni_program.c
+++ OPP_grupni_program.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <math.h>
-
 
 // grupni program OPP
 
@@ -17,12 +14,57 @@
 //na novo traži podatake o unosu hrane i vode, te zatim ispisuje sve podatke za drugi dan. 
 //Kada korisnik odabere kraj programa, ispisuje se još jedna poruka koja daje sumu svih unosa hrane i vode za sve protekle dane.
 
-int main(){
+
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+    int kcal, visina, godine;
+    float voda, tezina, bmr, kcal_min, kcal_max, casa_min, casa_max, voda_min, voda_max, vodal;
+    char spol;
+    
+        printf("Unesite svoju visinu \n");
+        scanf_s("%d", &visina);
+        printf("Unesite svoju tezinu \n");
+        scanf_s("%f", &tezina);
+        printf("Koliko imate godina \n");
+        scanf_s("%d", &godine);
+        printf("Unesite svoj spol (M/Z) \n");
+        scanf_s(" %c", &spol, sizeof(spol));
+        printf("Unesite kolicinu kalorije unesenih danas \n");
+        scanf_s("%d", &kcal);
+        printf("Unesite kolicinu vode unesene danas u ml \n");
+        scanf_s("%f", &voda);
+            voda_min = tezina * 0.033;
+            voda_max = tezina * 0.066;
+        if (spol == 'M') {
+            kcal_min = 10 * tezina + 6.25 * visina - 5 * godine + 5;
+            kcal_max = (13.397 * tezina + 4.799 * visina - 5.677 * godine + 88.362)*1.5;
+        }
+        else if (spol == 'Z') {
+            kcal_min = 10 * tezina + 6.25 * visina - 5 * godine - 161;
+            kcal_max = (9.247 * tezina + 3.098 * visina - 4.330 * godine + 447.593)*1.5;
+        }
+        vodal = voda / 1000;
+        if (kcal < kcal_min)
+            printf("Vas danasnji unos kalrija je premalen \n");
+        else if (kcal > kcal_min && kcal < kcal_max)
+            printf("Vas danasnji unos kalorija je dovoljan \n");
+        else if (kcal > kcal_max)
+            printf("Vas danasnji unos kalorija je prevelik \n");
+
+        if (vodal < voda_min)
+            printf("Vas danasnji unos vode je premalen \n");
+        else if (vodal > voda_min && vodal < voda_max)
+            printf("Vas danasnji unos vode je dovoljan \n");
+        else if (vodal > voda_max)
+            printf("Vas danasnji unos vode je prevelik \n");
+        printf("Minimalni i  maksimalni unos kalorija mora biti %.2f i %.2f \n", kcal_min, kcal_max);
+        printf("Minimalni i maksimalni unos vode mora biti %.2f l i %.2f l\n", voda_min, voda_max);
 
 
-
-
+    
+    return 0;
 }
-
 
 
