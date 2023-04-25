2f1344ba0e8dea8a02e29e1c341a10839ec83b73
ivorp95 <ivor.pangos@live.com>
2023-04-25 10:19:02 +0200

    modified:   asp_pred_samostalni.c

--- ASP_samostalni_rad/asp_pred_samostalni.c
+++ ASP_samostalni_rad/asp_pred_samostalni.c
@@ -1,3 +1,20 @@
+/*
+ASP samostalni zadatak 2023 Ivor Pangos
+
+Potrebno je izraditi program pisan u C jeziku u kojem se podaci (minimalno 3 komponente, pri tome: minimalno 1 komponenta tip polje znakova i minimalno 1 komponenta brojevnog tipa) pohranjuju u strukturu podataka tipa RED.
+Odabir načina implementacije reda (preko polja ili preko pokazivača, tj. ćelija), kao i vrste podataka za pohranu (npr. zbirka filmova, knjiga, cipela, ...) je na vama, tj. odabir je slobodan.
+Program treba imati početni izbornik u kojem korisnik odabire operaciju koju želi izvršiti:
+- unos novog zapisa,
+- ispis svih zapisa,
+- brisanje postojećeg zapisa,
+- izmjenu postojećeg zapisa,
+- pretraživanje reda prema vrijednosti jedne komponente i odgovarajući ispis. Napisana funkcija ispisuje sve elemente reda koji sadrže taj podatak. Primjer: ispisuju se svi podaci o svim ljubimcima koji se zovu Max, koliko god ljubimaca tog imena u redu bilo.
+Sve navedene funkcije je potrebno isprogramirati.
+C kod projektnog zadatka je potrebno predati do ponedjeljka 05.06.2023. u 08:00 sati
+
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
